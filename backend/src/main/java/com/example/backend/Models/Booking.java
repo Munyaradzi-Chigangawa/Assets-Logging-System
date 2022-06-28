f4e0d68288b3bf0d881161e54754565ebc44858b
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-06-28 12:21:36 +0200

    updated Booking Model

--- backend/src/main/java/com/example/backend/Models/Booking.java
+++ backend/src/main/java/com/example/backend/Models/Booking.java
@@ -1,2 +1,27 @@
-package com.example.backend.Models;public class Booking {
+package com.example.backend.Models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "bookings")
+
+public class Booking {
+
+    @Id
+    @Column(length = 100)
+    private String bookingId;
+    private String name;
+    private String serialNumber;
+    private String model;
+    private String quantity;
+    private String updatedAt;
+
 }
