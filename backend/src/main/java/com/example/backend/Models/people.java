5482c93f57ebe315fa26e98a06d50192efaa7dd5
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-06-28 20:46:44 +0200

    added people model

--- backend/src/main/java/com/example/backend/Models/people.java
+++ backend/src/main/java/com/example/backend/Models/people.java
@@ -1,2 +1,29 @@
-package com.example.backend.Models;public class people {
+package com.example.backend.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "people")
+public class people {
+    @Id
+    @Column(length = 100)
+    private String id;
+    private String name;
+    private String department;
+    private String designation;
+    private String phone;
+    private String email;
 }
+
+
