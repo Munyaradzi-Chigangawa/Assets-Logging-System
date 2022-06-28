1b6b166d1effc03f2c6c99a1de67941421da8aa9
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-06-28 12:21:53 +0200

    updated Booking Repository

--- backend/src/main/java/com/example/backend/Repositories/BookingRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/BookingRepository.java
@@ -1,2 +1,7 @@
-package com.example.backend.Repositories;public interface BookingRepository {
+package com.example.backend.Repositories;
+import com.example.backend.Models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, String> {
+
 }
