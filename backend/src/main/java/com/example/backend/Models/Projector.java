ea2370992cc039db34a3a3c7602668acd23df7fd
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-02 18:44:51 +0200

    fixed projector model

--- backend/src/main/java/com/example/backend/Models/Projector.java
+++ backend/src/main/java/com/example/backend/Models/Projector.java
@@ -1,13 +1,13 @@
 package com.example.backend.Models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -15,17 +15,20 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "projectors")
-
+@Builder
 public class Projector implements Serializable {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(length = 100)
-    private String id;
-    private String projector_name;
-    private String projector_model;
-    private String projector_number;
-//    private String booking_duration;
-//    private String created_at;
-//    private String updated_at;
+    private Long id;
+    @Column(name = "projector_name", columnDefinition = "varchar(255)")
+    private String projectorName;
+    private String projectorModel;
+    private String projectorNumber;
+    @CreationTimestamp
+    private String createdAt;
+    @UpdateTimestamp
+    private String updatedAt;
 }
 
