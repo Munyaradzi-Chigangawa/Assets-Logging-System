cb6543eda9d03e1b8e1b3ef7bb0fb2bcfc398fd5
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-02 18:44:06 +0200

    fixed bugs

--- backend/src/main/java/com/example/backend/Controllers/ProjectorController.java
+++ backend/src/main/java/com/example/backend/Controllers/ProjectorController.java
@@ -3,43 +3,48 @@ import com.example.backend.Models.Booking;
 import com.example.backend.Models.Projector;
 import com.example.backend.Repositories.BookingRepository;
 import com.example.backend.Repositories.ProjectorRepository;
+import com.example.backend.Services.ProjectorService;
+import com.example.backend.dto.ProjectorDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin(value = "*", allowedHeaders = "*")
+@RequiredArgsConstructor
+@Slf4j
 public class ProjectorController {
-    @Autowired
-    private ProjectorRepository projectorRepository;
+    private final ProjectorService projectorService;
 
-    @RequestMapping ( value = "/getProjectors", method = RequestMethod.GET, produces = "application/json")
-    public List <Projector> getProjectors() {
-        List<Projector> list = new ArrayList<>();
-        projectorRepository.findAll().forEach(list::add);
-        return list;
-    }
+//    @RequestMapping ( value = "/getProjectors", method = RequestMethod.GET, produces = "application/json")
+//    public List <Projector> getProjectors() {
+//        List<Projector> list = new ArrayList<>();
+//        projectorRepository.findAll().forEach(list::add);
+//        return list;
+//    }
 
-    @RequestMapping ( value = "/count", method = RequestMethod.GET, produces = "application/json")
-    public int noOfProjectors() {
-        return (int) projectorRepository.count();
-    }
+//    @RequestMapping ( value = "/count", method = RequestMethod.GET, produces = "application/json")
+//    public int noOfProjectors() {
+//        return (int) projectorRepository.count();
+//    }
 
-    @RequestMapping ( value = "/addProjector", method = RequestMethod.POST, produces = "application/json")
-    public void addProjector (@RequestBody List<Projector> projectors) {
-        System.out.println("Added Projector");
-        projectorRepository.saveAll(projectors);
+    @PostMapping( value = "/addProjector")
+    public ResponseEntity<Projector> addProjector (@RequestBody ProjectorDto projector) {
+        log.info("Projector Successfully Added.");
+        return new ResponseEntity<>(projectorService.save(projector), HttpStatus.CREATED);
     }
-    @RequestMapping ( value = "/deleteProjector", method = RequestMethod.POST, produces = "application/json")
-    public void deleteProjector (@RequestBody List<Projector> projectors) {
-        System.out.println("Deleted Projector");
-        projectorRepository.deleteAll(projectors);
-    }
+//    @RequestMapping ( value = "/deleteProjector", method = RequestMethod.POST, produces = "application/json")
+//    public void deleteProjector (@RequestBody List<Projector> projectors) {
+//        System.out.println("Deleted Projector");
+//        projectorRepository.deleteAll(projectors);
+//    }
 
 
 //    @RequestMapping ( value = "/cancelProjectorBooking", method = RequestMethod.POST, produces = "application/json")
