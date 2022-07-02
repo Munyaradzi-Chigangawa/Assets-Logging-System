342bb9fccac914edd2ad2b18aa689988de1fdbe8
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-02 18:43:28 +0200

    added Projector Service

--- backend/src/main/java/com/example/backend/Services/ProjectorService.java
+++ backend/src/main/java/com/example/backend/Services/ProjectorService.java
@@ -1,2 +1,28 @@
-package com.example.backend.Services;public class ProjectorService {
+package com.example.backend.Services;
+
+import com.example.backend.Models.Projector;
+import com.example.backend.Repositories.ProjectorRepository;
+import com.example.backend.dto.ProjectorDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ProjectorService {
+    @Autowired
+    private final ProjectorRepository projectorRepository;
+
+    public Projector save (ProjectorDto projector) {
+        Projector projector1 = Projector.builder()
+                .projectorModel(projector.getProjector_model())
+                .projectorName(projector.getProjector_name())
+                .projectorNumber(projector.getProjector_number())
+                .build();
+        return projectorRepository.save(projector1);
+    }
+
 }
