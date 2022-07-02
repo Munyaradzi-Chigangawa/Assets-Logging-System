ce9ede4add8053ae932d23627a8aabe524515969
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-02 18:45:13 +0200

    fixed projector repository

--- backend/src/main/java/com/example/backend/Repositories/ProjectorRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/ProjectorRepository.java
@@ -3,6 +3,6 @@ package com.example.backend.Repositories;
 import com.example.backend.Models.Projector;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectorRepository extends JpaRepository <Projector, String> {
+public interface ProjectorRepository extends JpaRepository <Projector, Long> {
 
 }
