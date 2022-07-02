2078fa2ecc18198c79e40070355b4af245ef4e56
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-02 18:43:01 +0200

    added Projector Dto

--- backend/src/main/java/com/example/backend/dto/ProjectorDto.java
+++ backend/src/main/java/com/example/backend/dto/ProjectorDto.java
@@ -1,2 +1,20 @@
-package com.example.backend.dto;public class projectorDto {
+package com.example.backend.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class ProjectorDto implements Serializable {
+    @NotEmpty(message = "Projector Name is Required.")
+    private String projector_name;
+    @NotEmpty(message = "Projector Model is Required.")
+    private String projector_model;
+    @NotEmpty(message = "Projector Serial Number is Required.")
+    private String projector_number;
+
 }
