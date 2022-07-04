712ccd032ac2e45766304a0d4cfca55659c2115c
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-04 11:22:55 +0200

    changed data type to Long

--- backend/src/main/java/com/example/backend/Repositories/DepartmentRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/DepartmentRepository.java
@@ -2,6 +2,6 @@ package com.example.backend.Repositories;
 import com.example.backend.Models.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DepartmentRepository extends JpaRepository <Department, String>{
+public interface DepartmentRepository extends JpaRepository <Department, Long>{
 
 }
