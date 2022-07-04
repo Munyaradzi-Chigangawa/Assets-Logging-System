ee17b0066feaca0e8f4f8323227ab8b6def70c89
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-04 11:23:54 +0200

    added departmentDto

--- backend/src/main/java/com/example/backend/dto/DepartmentDto.java
+++ backend/src/main/java/com/example/backend/dto/DepartmentDto.java
@@ -1,2 +1,26 @@
-package com.example.backend.dto;public class DepartmentDto {
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DepartmentDto implements Serializable {
+    private Long id;
+    @NotEmpty(message = "Department Name is Required.")
+    private String departmentName;
+    @NotEmpty(message = "Department Code is Required.")
+    private String departmentCode;
+    @NotEmpty(message = "Department Description is Required.")
+    private String departmentDescription;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 }
