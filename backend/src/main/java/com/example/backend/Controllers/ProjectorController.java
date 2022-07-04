da9305030885089c617682c62ca9e01879906e6b
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-04 11:19:26 +0200

    fixed department controller

--- backend/src/main/java/com/example/backend/Controllers/ProjectorController.java
+++ backend/src/main/java/com/example/backend/Controllers/ProjectorController.java
@@ -7,12 +7,9 @@ import com.example.backend.Services.ProjectorService;
 import com.example.backend.dto.ProjectorDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
