9586eab6305c2cc75bb3c28d509d69800e0071cb
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-05 05:09:44 +0200

    added routing code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './reusable/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'admin', 
+    pathMatch: 'full'
+ },
+  { path : 'admin', loadChildren: ()=> 
+import('../app/admin/admin.module').then(mod => mod.AdminModule)
+},
+{ path : '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
