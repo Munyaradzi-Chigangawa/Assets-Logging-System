0cb295814f60385c60a57261ec98b97b1235edb7
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-05 05:06:37 +0200

    added shared files

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SanitizeHtmlPipe } from './sanitize-html.pipe';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CKEditorModule } from 'ckeditor4-angular';
+import { TooltipModule } from 'ng2-tooltip-directive';
+
+
+
+@NgModule({
+  declarations: [SanitizeHtmlPipe],
+  imports: [
+    CommonModule,
+    NgxPaginationModule,
+    CKEditorModule,
+    TooltipModule
+  ],
+  exports: [SanitizeHtmlPipe, NgxPaginationModule, CKEditorModule]
+})
+export class SharedModule { }
