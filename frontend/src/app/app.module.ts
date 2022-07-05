7d77dc3c63337469e93cbcd5394728f1bdacdaf4
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-05 05:10:25 +0200

    added declarations

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './reusable/page-not-found/page-not-found.component';
+import { AdminRoutingModule } from './admin/admin-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
