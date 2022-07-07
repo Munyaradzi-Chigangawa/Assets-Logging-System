f525b163752c01d5efb13841f0be84247c2f3838
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-07 16:37:57 +0200

    added model

--- frontend/src/app/pages/people/people.component.ts
+++ frontend/src/app/pages/people/people.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { People } from 'src/app/models/people';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
 })
 export class PeopleComponent implements OnInit {
+
+  people: People[];
 
   constructor() { }
 
