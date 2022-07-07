63b58d422a23d2e90673d4d3ac0f0a04ae42095c
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-07 16:38:31 +0200

    added backend to front

--- frontend/src/app/pages/laptops/laptops.component.ts
+++ frontend/src/app/pages/laptops/laptops.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Laptop } from 'src/app/models/laptop';
 
 @Component({
   selector: 'app-laptops',
   templateUrl: './laptops.component.html',
 })
 export class LaptopsComponent implements OnInit {
+
+  laptops: Laptop[];
 
   constructor() { }
 
