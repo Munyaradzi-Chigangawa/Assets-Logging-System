e8d63a3080c8a2f3dc586d47b029e759896224d1
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-07 17:45:02 +0200

    fixed backend data for department

--- frontend/src/app/pages/departments/departments.component.ts
+++ frontend/src/app/pages/departments/departments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Department } from 'src/app/models/department';
+import { DepartmentService } from 'src/app/services/department.service';
 
 
 @Component({
@@ -10,9 +11,20 @@ export class DepartmentsComponent implements OnInit {
 
   departments: Department[];
 
-  constructor() { }
+  constructor(private departmentService: DepartmentService) { }
 
   ngOnInit(): void {
+    this.getDepartments();
+  }
+
+  private getDepartments() {
+    this.departmentService
+    .getDepartmentsList()
+    .subscribe(
+    data => {
+      this.departments = data
+    console.log(data)},
+    );
   }
 
 }
