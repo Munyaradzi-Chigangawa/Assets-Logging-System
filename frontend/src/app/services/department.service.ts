65c640dd69992602581ace01d9aff45e787bbcfc
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-08 21:27:08 +0200

    added department

---
+++ frontend/src/app/services/department.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Department } from '../models/department';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+
+  private baseUrl = 'http://localhost:8080/api/getDepartments';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getDepartmentsList() : Observable<Department[]> {
+    return this.httpClient.get<Department[]>(`${this.baseUrl}`);
+  }
+}
