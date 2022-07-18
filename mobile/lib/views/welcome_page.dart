eac03012583edadda124482477a53d20c3b9fc81
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-18 09:58:56 +0200

    welcome page working

--- mobile/lib/views/welcome_page.dart
+++ mobile/lib/views/welcome_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants/button.dart';
+
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
@@ -10,12 +12,86 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        gi
-          child: Text('Welcome'),
-        ),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(),
+        body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Image(
+                      image: AssetImage('assets/img/logo.png'),
+                          height: 130,
+                        ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                'Welcome to',
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text('CUT ICT Assets',
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black)),
+                            //  AppName(fontSize: 25),
+                            ],
+                          ),
+                          const Padding(
+                            padding:
+                                EdgeInsets.only(left: 30, right: 30, top: 5),
+                            child: Text(
+                              'Please sign in to continue',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  )),
+              const Spacer(),
+              const SizedBox(
+                height: 30,
+              ),
+              //Text("don't have social accounts?") ,
+              BigButton(
+                // child: Text('Authentication >>>', style: TextStyle(color: Theme.of(context).primaryColor),),
+                onPressed: () {},
+                child: const Text(
+                  'Sign In',
+                  //style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+              ),
+
+              const SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
+        ));
   }
 }
